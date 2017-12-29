ad09834ba6e5d6d449382d79a1417043446ca439
Royz2123 <royzohar25@gmail.com>
2017-12-29 14:10:25 +0200

    Added some plots

--- q1/est_parms.py
+++ q1/est_parms.py
@@ -1,3 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.mlab as mlab
+
 import math
 import random
 import sys
@@ -21,25 +25,29 @@ def est_mean(samples):
 def est_std_dev(samples, est_mean):
     return math.sqrt(sum(map(lambda x: (x - est_mean)**2, samples)) / len(samples))
 
-
-def main():
-    if len(sys.argv) < 2:
-        print("Usage: python est_parms.py [FILENAME]")
-
-    samples = get_samples(sys.argv[1])
+def est_parms(filename):
+    samples = get_samples(filename)
 
     # estimate parms
     mean = est_mean(samples)
     std_dev = est_std_dev(samples, mean)
 
-    # print results
-    print("""
-    Estimated parameters:
-    MEAN:               %f
-    STANDARD DEVIATION: %f
-    """ % (
-        mean, std_dev
-    ))
+    # return values
+    return mean, std_dev
+
+
+def main():
+    if len(sys.argv) < 3:
+        print("Usage: python est_parms.py [FILENAME1] [FILENAME2] ")
+
+    mean1, std_dev1 = est_parms(sys.argv[1])
+    mean2, std_dev2 = est_parms(sys.argv[2])
+
+    # plot the results
+    x = np.linspace(-5, 5, 100)
+    plt.plot(x,mlab.normpdf(x, mean1, std_dev1))
+    plt.plot(x,mlab.normpdf(x, mean2, std_dev2))
+    plt.show()
 
 
 
