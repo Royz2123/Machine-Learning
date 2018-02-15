37297117b4d1afef38c5bbe2fabca89613de1ac5
Royz2123 <royzohar25@gmail.com>
2018-02-15 19:38:14 +0200

    Fixed maz svm

--- HW3/q3/svm.py
+++ HW3/q3/svm.py
@@ -6,7 +6,8 @@ import numpy as np
 from sklearn import svm
 import matplotlib.pyplot as plt
 
-CLASSES=10
+CLASSES = 10
+FOLDS = 5
 
 def read_samples(filename):
     samples = []
@@ -21,7 +22,6 @@ def read_samples(filename):
             samples.append([float(v) for v in buf[0:-1].split(',')])
     return samples
 
-
 def extract_label(set):
     labels = []
     trainSet = []
@@ -30,10 +30,10 @@ def extract_label(set):
         trainSet.append(set[i][1:])
     return trainSet,labels
 
+#divide into k fold crossVal
 def k_fold(set,k,c=1,g=-1):
     random.shuffle(set)
     errors = []
-    #print set
     test = []
     size = len(set)/k
     for i in range(k):
@@ -53,6 +53,7 @@ def k_fold(set,k,c=1,g=-1):
 def SVM_Error(trainSet, testSet, c, g):
     return oneVsAll(trainSet,testSet,c,g)
 
+#train in One Against All
 def oneVsAll(trainSet,testSet,c,g):
     fits = []
     trainSet, trainLabels = extract_label(trainSet)
@@ -60,38 +61,30 @@ def oneVsAll(trainSet,testSet,c,g):
     testLabels = np.asarray(testLabels).ravel()
     resultSet = []
     for i in range(CLASSES):
-        #print CLASSES-i
-        #print trainLabels
         labels = oneVsAll_labeling(trainLabels,i+1)
         labels = np.asarray(labels).ravel()
-        #print labels
 
         if g != -1 :
             clf = svm.SVC(C=c, gamma=g, kernel='rbf')
-            print g,c
         else:
             clf = svm.SVC(C=c, kernel='linear')
-            print -1,c
         if len(set(labels)) != 1:
             #do something ..
-            #return 0.5
             clf.fit(np.asarray(trainSet),labels)
             fits.append(clf)
 
     for i in range(len(testSet)):
         yi = [testSet[i]]
-        max = -109
+        max = None
         maxj = -1
         for j in range(len(fits)):
-            #print fits[j].decision_function(yi)
-            if(fits[j].decision_function(yi)>max or max==-109):
+            if(fits[j].decision_function(yi)>max or max==None):
                 max = fits[j].decision_function(yi)
                 maxj = j
         resultSet.append(maxj+1)
-        #print "hey", maxj
     return ErrorCompare(resultSet,testLabels)
 
-
+#label one class as 1 and all the others as 0
 def oneVsAll_labeling(set, rightClass):
     labels = []
     trainSet = []
@@ -102,10 +95,8 @@ def oneVsAll_labeling(set, rightClass):
             labels.append([0])
     return labels
 
-
+#check error rate of one fold
 def ErrorCompare(rs, tl):
-    print rs
-    print tl
     sum = 0.
     for i in range(len(rs)):
         if rs[i] == tl[i]:
@@ -114,88 +105,42 @@ def ErrorCompare(rs, tl):
 
 
 #train linear SVM
-
 def trainLinearSVM (X,K,allC):
     Cerrors = []
     for c in allC:
-        #for g in allG:
         errors = k_fold(X,K,c)
+        #calc mean error of kCrossFold
         Cerrors.append(sum(errors) / float(len(errors)))
-    print Cerrors
+    printErrors (Cerrors,allC)
 
-
+#train RBFS SVM
 def trainRBFSVM (X,K,allC, allG):
     CGerrors = []
     for c in allC:
         for g in allG:
-            #for g in allG:
             errors = k_fold(X,K,c,g)
             CGerrors.append(sum(errors) / float(len(errors)))
-    print CGerrors
+    printErrors (CGerrors,allC,allG)
+
+
+
+def printErrors(errList,clist,glist=[]):
+    x=0
+    for i in clist:
+        if len(glist)>0:
+            for j in glist:
+                print ("c=",i,"g=", j, " error =", errList[x])
+                x+=1
+        else:
+            print ("c=",i, " error =", errList[x])
+            x+=1
 
 def main():
-    train = read_samples("csv/train.csv")
-    trainLinearSVM(train,5,[1, 5])
-    print ""
-    trainRBFSVM(train,5,[1, 5],[0.005, 0.01, 0.05])
+    path = sys.argv[1] # here "csv/train.csv"
+    train = read_samples(path)
+    trainLinearSVM(train,FOLDS,[1, 5])
+    trainRBFSVM(train,FOLDS,[1, 5],[0.005, 0.01, 0.05])
 
 
 if __name__ == "__main__":
 	main()
-
-
-
-
-
-
-##      graveyard
-
-
-
-
-def divide_samples(set, rightClass):
-    pos = []
-    neg = []
-    for i in range(len(set)):
-        if set[i][0] == rightClass :
-            pos.append(set[i][1:])
-        else :
-            neg.append(set[i][1:])
-    return pos,neg
-
-
-def oneVsAll_labeling(set, rightClass):
-    labels = []
-    trainSet = []
-    for i in range(len(set)):
-        if set[i][0] == rightClass :
-            labels.append([0])
-        else :
-            labels.append([1])
-        trainSet.append(set[i][1:])
-    return trainSet,labels
-
-
-
-def trainAndTest(trainSet, testSet, num, c, g):
-    trainSet,labels = oneVsAll_labeling(trainSet)
-    resultSet = []
-    if g != -1 :
-        clf = svm.SVC(C=c, gemma=g, kernel='rbf')
-    else:
-        clf = svm.SVC(C=c, kernel='linear')
-
-    labels = np.asarray(labels).ravel()
-
-    if len(set(labels)) == 1:
-        clf.fit(np.asarray(trainSet), np.asarray(labels))
-        testSet,testLabels = oneVsAll_labeling(testSet, 2)
-        resultSet = clf.predict(testSet)
-        testLabels = np.asarray(testLabels)
-        testLabels = testLabels.ravel()
-        print(resultSet)
-        print(testLabels)
-        return resultSet,testLabels
-    else:
-        print("all the same type ..")
-    return [],[]
