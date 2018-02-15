78a04f6b8bae894581f5f2619661ed7005ba05cc
Royz2123 <royzohar25@gmail.com>
2018-02-15 19:10:46 +0200

    Finished coding q1

--- HW3/q1/pca.py
+++ HW3/q1/pca.py
@@ -18,30 +18,64 @@ def read_text(f_name):
     with open(f_name, 'r') as f:
         for line in f.readlines():
             data.append(np.array([float(var) for var in line.split(",")]))
+    print(len(data))
     return np.array(data)
 
 
 # normalize a set of training samples
 def normalize(data):
+    # normalize to mean 0 and variance 1 (axis 1 is the rows)
+    data = preprocessing.scale(data, axis=1)
+
     # compute the mean vector
     data -= data.mean(axis=0)
 
-    # normalize to mean 0 and variance 1 (axis 1 is the rows)
-    # data = preprocessing.scale(data, axis=1)
 
-
+def faces_mean(face1, face2):
+    return (face1 + face2) * 0.5
 
 
 # Given normalized data find the optimal projection
 def find_opt_projection(data):
     # compute scatter matrix
-    scatter_matrix = np.cov(data, rowvar=False)
+    # scatter_matrix = np.cov(data, rowvar=False)
+    scatter_matrix = np.dot(data.T, data)
 
     # compute eigenvalues and eigenvectors
     d, u = np.linalg.eigh(scatter_matrix)
 
     # w_pca projection matrix
     w_pca = np.asarray(u).T[::-1]
+
+    # compute ni, mu_i, mu
+    ni = 2
+    mu_is = np.array([
+        faces_mean(data[i], data[i+1])
+        for i in range(0, len(data), 2)
+    ])
+    mu = mean(data, axis=0)
+
+    # calculate s_w
+    subs = [
+        data[i] - mu_is[i/2]
+        for i in range(100)
+    ]
+    s_w = sum([
+        np.dot(vec, vec.T)
+        for vec in subs
+    ])
+
+    # calculate s_b
+    s_b = sum([
+        ni*(class_mean - mu)*(class_mean - mu).T
+        for class_mean in mu_is
+    ])
+
+    # calculate w_mda
+    d, u = np.linalg.eigh(s_b, s_w)
+    w_pca = np.asarray(u).T[::-1]
+
+    return w_mda * w_pca.T
 
 
 
@@ -54,8 +88,8 @@ def train(train_data, w_opt):
 
     # compute model for each person as average of 2 faces:
     modeled_faces = []
-    for i in range(0, len(train_data), 2)
-        modeled_faces.append((train_data[i] + train_data[i+1])*0.5)
+    for i in range(0, len(train_data), 2):
+        modeled_faces.append(faces_mean(train_data[i], train_data[i+1]))
 
     # return the projected faces
     return np.array(modeled_faces) * w_opt
@@ -84,7 +118,7 @@ def main():
     test_data = read_text(TEST_PATH)
 
     # find the optimal projection on training data
-    w_opt = find_opt_projection(data)
+    w_opt = find_opt_projection(train_data)
 
     # train based on training_data
     proj_faces = train(train_data, w_opt)
