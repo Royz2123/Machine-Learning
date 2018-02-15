a1f9e3aac59ec1a07d9922b94573a8fb906fe67a
Royz2123 <royzohar25@gmail.com>
2018-02-15 19:16:13 +0200

    Finished q1!

--- HW3/q1/pca.py
+++ HW3/q1/pca.py
@@ -4,6 +4,7 @@ import matplotlib.mlab as mlab
 from sklearn import linear_model, metrics
 from sklearn import preprocessing
 from scipy import spatial
+import scipy
 
 import math
 import random
@@ -18,7 +19,6 @@ def read_text(f_name):
     with open(f_name, 'r') as f:
         for line in f.readlines():
             data.append(np.array([float(var) for var in line.split(",")]))
-    print(len(data))
     return np.array(data)
 
 
@@ -30,102 +30,98 @@ def normalize(data):
     # compute the mean vector
     data -= data.mean(axis=0)
 
+    return data
+
 
 def faces_mean(face1, face2):
     return (face1 + face2) * 0.5
 
 
-# Given normalized data find the optimal projection
-def find_opt_projection(data):
+def find_pca(data):
     # compute scatter matrix
     # scatter_matrix = np.cov(data, rowvar=False)
-    scatter_matrix = np.dot(data.T, data)
+    scatter_matrix = np.dot(data, data.T)
 
     # compute eigenvalues and eigenvectors
-    d, u = np.linalg.eigh(scatter_matrix)
+    d, u = scipy.linalg.eigh(scatter_matrix, eigvals=(50, len(data)-1))
 
     # w_pca projection matrix
-    w_pca = np.asarray(u).T[::-1]
+    w_pca = data.T.dot(np.asarray(u[::-1])).dot(np.diag(d))
+
+    return w_pca
+
+
+# Given normalized data find the optimal projection
+def find_mda(data):
+    proj_data = data.dot(find_pca(data))
 
     # compute ni, mu_i, mu
     ni = 2
     mu_is = np.array([
-        faces_mean(data[i], data[i+1])
-        for i in range(0, len(data), 2)
+        faces_mean(proj_data[i], proj_data[i+1])
+        for i in range(0, len(proj_data), 2)
     ])
-    mu = mean(data, axis=0)
+    mu = np.mean(proj_data, axis=0)
 
     # calculate s_w
-    subs = [
-        data[i] - mu_is[i/2]
+    subs = np.array([
+        proj_data[i] - mu_is[int(i/2)]
         for i in range(100)
-    ]
-    s_w = sum([
-        np.dot(vec, vec.T)
-        for vec in subs
     ])
+    s_w = np.zeros((50,50), dtype = 'float32')
+    for i in range(subs.shape[0]):
+        s_w = np.add(s_w, np.outer(subs[i].T, subs[i]))
 
     # calculate s_b
-    s_b = sum([
-        ni*(class_mean - mu)*(class_mean - mu).T
-        for class_mean in mu_is
-    ])
+    s_b = np.zeros((50,50), dtype = 'float32')
+    for i in range(len(mu_is)):
+        s_b = np.add(s_b, ni * np.outer((mu_is[i] - mu), (mu_is[i] - mu).T))
 
-    # calculate w_mda
-    d, u = np.linalg.eigh(s_b, s_w)
-    w_pca = np.asarray(u).T[::-1]
-
-    return w_mda * w_pca.T
+    # calculate w_mda using generalized egien value
+    d, u = scipy.linalg.eig(a=s_b, b=s_w)
+    return np.asarray(u).T[::-1]
 
 
-
-def train(train_data, w_opt):
-    # first normalize the data
-    normalize(train_data)
-
-    # project the training samples to w_opt
-    train_data = train_data * w_opt
-
-    # compute model for each person as average of 2 faces:
-    modeled_faces = []
-    for i in range(0, len(train_data), 2):
-        modeled_faces.append(faces_mean(train_data[i], train_data[i+1]))
-
-    # return the projected faces
-    return np.array(modeled_faces) * w_opt
-
-
-def classify(test_data, proj_faces, w_opt):
+def classify(model, test_data, w_pca, w_opt):
     # first normalize the data
     normalize(test_data)
 
     # project the test sampels to w_opt
-    test_data = test_data * w_opt
+    test_data = test_data.dot(w_pca).dot(w_opt.T)
 
     # test using nearest neighbour
-    tree = spatial.KDTree(proj_faces)
+    tree = spatial.KDTree(model)
     hits = sum([
         index == tree.query(face)[1]
         for index, face in enumerate(test_data)
     ])
 
     # output the success rate
-    return hits / len(test_data)
+    return float(hits) / float(test_data.shape[0])
 
 def main():
     # read the data
     train_data = read_text(TRAIN_PATH)
     test_data = read_text(TEST_PATH)
 
-    # find the optimal projection on training data
-    w_opt = find_opt_projection(train_data)
+    # normalize data firs
+    train_data = normalize(train_data)
+    test_data = normalize(test_data)
 
-    # train based on training_data
-    proj_faces = train(train_data, w_opt)
+    # find the optimal projection on training data
+    w_pca = find_pca(train_data)
+    w_opt = find_mda(train_data)
+
+    # find training model
+    model = np.array([
+        faces_mean(train_data[i], train_data[i+1])
+        for i in range(0, len(train_data), 2)
+    ])
+    model = model.dot(w_pca).dot(w_opt.T)
 
     # classify based on testing samples
-    success_rate = classify(test_data, proj_faces, w_opt)
-    print ("Success rate:\t%d" % success_rate)
+    success_rate = classify(model, test_data, w_pca, w_opt)
+    print ("Success rate:\t%f" % success_rate)
 
 if __name__ == "__main__":
 	main()
